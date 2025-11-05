adbc7a991b92540ac2b5502f701e023dea2848ef
SalsaCat <laugessvan@gmail.com>
2025-11-05 14:39:51 +0100

    Remove comments

--- commands/ping.js
+++ commands/ping.js
@@ -1,14 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    // Defines the command name, description, and any options
     data: new SlashCommandBuilder()
-        .setName('ping') // This is what the user types: /ping
         .setDescription('Replies with Pong!'),
 
-    // The execution function uses interaction instead of message
     async execute(interaction) {
-        // Reply is always used for interactions
         await interaction.reply('Pong!');
     },
 };

55d256412d7157de4ec1fcdf222be8c08ad9eb34
SalsaCat <laugessvan@gmail.com>
2025-11-05 14:25:03 +0100

    Get commands

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const path = require('path');
 const TOKEN = process.env.BOT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(path.join(commandsPath, file));
+	commands.push(command.data.toJSON());
+}

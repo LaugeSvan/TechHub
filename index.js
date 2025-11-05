60c20ea76dc1255a94e7c3eac5dbac68f16837d7
SalsaCat <laugessvan@gmail.com>
2025-11-05 14:23:49 +0100

    Import the rest

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 require('dotenv').config();
+
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;

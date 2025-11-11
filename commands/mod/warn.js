4a0ff67573556f59fbd7f6860cd0c2b592d16a8f
SalsaCat <laugessvan@gmail.com>
2025-11-11 15:42:03 +0100

    Add choosing of timeout duration

--- commands/mod/warn.js
+++ commands/mod/warn.js
@@ -2,7 +2,18 @@ const { SlashCommandSubcommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
 
 const MOD_LOG_CHANNEL_ID = '1434814467119779880';
-const TIMEOUT_DURATION_MS = 60 * 60 * 1000;
+
+const TIMEOUT_DURATIONS = {
+    '5 minutes': 5 * 60 * 1000,
+    '15 minutes': 15 * 60 * 1000,
+    '30 minutes': 30 * 60 * 1000,
+    '1 hour': 60 * 60 * 1000,
+    '6 hours': 6 * 60 * 60 * 1000,
+    '1 day': 24 * 60 * 60 * 1000,
+    '1 week': 7 * 24 * 60 * 60 * 1000,
+};
+const DEFAULT_DURATION_NAME = '1 hour';
+const DEFAULT_DURATION_MS = TIMEOUT_DURATIONS[DEFAULT_DURATION_NAME];
 
 const subcommandData = new SlashCommandSubcommandBuilder()
     .setName('warn')
@@ -14,7 +25,17 @@ const subcommandData = new SlashCommandSubcommandBuilder()
     .addStringOption(option =>
         option.setName('reason')
             .setDescription('The reason for the warning.')
-            .setRequired(true));
+            .setRequired(true))
+    .addStringOption(option => {
+        option.setName('duration')
+            .setDescription(`The duration of the timeout (Defaults to ${DEFAULT_DURATION_NAME}).`)
+            .setRequired(false);
+        
+        for (const [name, ms] of Object.entries(TIMEOUT_DURATIONS)) {
+            option.addChoices({ name: name, value: String(ms) });
+        }
+        return option;
+    });
 
 module.exports = {
     data: subcommandData,
@@ -24,6 +45,10 @@ module.exports = {
 
         const targetUser = interaction.options.getUser('target');
         const reason = interaction.options.getString('reason');
+        const durationChoice = interaction.options.getString('duration');
+        const timeoutDurationMS = durationChoice ? parseInt(durationChoice, 10) : DEFAULT_DURATION_MS;
+        const durationName = Object.keys(TIMEOUT_DURATIONS).find(key => TIMEOUT_DURATIONS[key] === timeoutDurationMS) || `${timeoutDurationMS / 3600000} hours`;
+        
         const targetMember = interaction.guild.members.cache.get(targetUser.id);
 
         if (!targetMember) {
@@ -47,24 +72,25 @@ module.exports = {
                     { name: 'Moderator', value: interaction.user.tag, inline: true },
                     { name: 'Reason', value: reason, inline: false }
                 )
-                .setFooter({ text: `A ${TIMEOUT_DURATION_MS / (60 * 1000)} minute timeout has also been applied.` })
+                .setFooter({ text: `A ${durationName} timeout has also been applied.` })
                 .setTimestamp();
                 
             await targetUser.send({ embeds: [dmEmbed] }).catch(() => {
                 console.log(`error.dmUser.${targetUser.tag}.`);
             });
 
-            await targetMember.timeout(TIMEOUT_DURATION_MS, reason);
+            await targetMember.timeout(timeoutDurationMS, reason);
 
             const replyEmbed = new EmbedBuilder()
                 .setColor('#00AA00')
-                .setDescription(`✅ Successfully **warned** and applied a **${TIMEOUT_DURATION_MS / (60 * 1000)} minute timeout** to ${targetUser.tag}. \nReason: *${reason}*`);
+                // --- UPDATE REPLY TO USE SELECTED DURATION NAME ---
+                .setDescription(`✅ Successfully **warned** and applied a **${durationName} timeout** to ${targetUser.tag}. \nReason: *${reason}*`);
             
             await interaction.editReply({ embeds: [replyEmbed] });
 
             const logChannel = interaction.guild.channels.cache.get(MOD_LOG_CHANNEL_ID);
             if (logChannel && logChannel.type === ChannelType.GuildText) {
-                logChannel.send(`**[MOD LOG - WARN]** ${targetUser.tag} was warned and timed out by ${interaction.user.tag}. Reason: ${reason}`);
+                logChannel.send(`**[MOD LOG - WARN]** ${targetUser.tag} was warned and timed out for **${durationName}** by ${interaction.user.tag}. Reason: ${reason}`);
             } else {
                 console.warn('error.modLogChannelNotFound');
             }
